c07b195994200e4bc8fe9d21ade8a6ef8ad13e7d
YskvAnn <yaskovaann2002@mail.ru>
2025-01-07 22:39:03 +0700

    initial

---
+++ tests/first_test.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Получаем путь к ChromeDriver с помощью WebDriverManager
+driver_path = ChromeDriverManager().install()
+
+# Создаем объект Service
+service = Service(driver_path)
+
+# Инициализируем WebDriver с использованием Service
+driver = webdriver.Chrome(service=service)
+
+# Открываем сайт
+driver.get("https://a.metaforce.app/news-portal/en")
+
+# Выводим заголовок страницы
+print(driver.title)
+
+# Закрываем браузер
+driver.quit()
