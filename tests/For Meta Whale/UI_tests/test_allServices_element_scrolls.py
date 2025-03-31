e88e5354d1192b5d8ae050ad456487d7d4294059
YskvAnn <yaskovaann2002@mail.ru>
2025-03-31 23:50:44 +0700

    initial

---
+++ tests/For Meta Whale/UI_tests/test_allServices_element_scrolls.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://b.metaforce.app/")
+
+# нахожу и кликаю на кнопку + время на прокрутку
+scroll_button = driver.find_element(By.XPATH, "/html/body/div/div/div/header/ul/li[6]")
+scroll_button.click()
+time.sleep(10)
+
+# h2
+target_element = driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/section[7]/div[1]/picture/img")
+
+current_scroll_position = driver.execute_script("return window.scrollY") # получаю текущую позицию страницы
+element_position = target_element.location['y']
+driver.execute_script("arguments[0].scrollIntoView();", target_element)
+time.sleep(5)
+
+# проверяю, что текущая позиция окна рядом с искомым элементов
+element_position = driver.execute_script("return window.scrollY")
+
+# проверяю, что позиция после прокрутки изменилась и стала соответствовать целевому элементу
+assert abs(element_position - element_position) < 10, f"Ожидаемая прокрутка не произошла! Позиция: {element_position}, Ожидаемая: {element_position}"
+
+print("Тест пройден успешно!")
+driver.quit()
---
+++ tests/For Meta Whale/UI_tests/test_botton_connectWallet.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://b.metaforce.app/")
+time.sleep(5)
+# нахожу и кликаю на кнопку + время на прокрутку
+scroll_button = driver.find_element(By.CSS_SELECTOR, "button.btn-group__btn.btn-connect")
+scroll_button.click()
+time.sleep(5)
+
+text_h2 = driver.find_element(By.CSS_SELECTOR, "div.modal-title")
+
+# получаю текст элемента
+element_text = text_h2.text
+if element_text == "Connect wallet":
+    print("Заголовок совпадает с 'Connect wallet'")
+else:
+    print(f"Заголовок не совпадает! Найдено: {element_text}")
+
+driver.quit()
